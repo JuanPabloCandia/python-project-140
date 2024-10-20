ad9c4d323722f5e3325370c39de99889e170b2d6
Juan Pablo Candia Tavera <jcandiatavera@gmail.com>
2024-10-20 06:26:57 +0000

    Add welcome user functionality and prompt dependency

---
+++ brain_games/cli.py
@@ -0,0 +1,7 @@
+# brain_games/cli.py
+from prompt_toolkit import prompt
+
+def welcome_user():
+    print("Welcome to the Brain Games!")
+    name = prompt("May I have your name? ")
+    print(f"Hello, {name}!")
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,9 @@
-# brain_games/scripts/brain_games.py
+#!/usr/bin/env python3
+
+from brain_games.cli import welcome_user
 
 def main():
-    print("Welcome to the Brain Games!")
+    welcome_user()  # Llama a la función de bienvenida
 
 if __name__ == "__main__":
     main()
