6b25616d52d8a55d8eae22aaca5d39bf8326cef3
Juan Pablo andia Tavera <jcandiatavera@gmail.com>
2025-04-07 18:40:04 -0500

    cambio de ruta

--- spec/10_brain_games_spec.rb
+++ spec/10_brain_games_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
-RSpec.describe 'bin/brain-games', type: :aruba do
+RSpec.describe 'lib/brain-games', type: :aruba do
   before(:each) do
-    file_path = File.expand_path('../code/brain_games/scripts/brain-games', __dir__)
+    file_path = File.expand_path('../.venv/lib/brain-games', __dir__)
     expect(File).to exist(file_path)
     run_command file_path
   end
-
   before(:each) do
     expect(last_command_started).not_to be_stopped
     type('Tirion')
@@ -15,8 +14,8 @@ RSpec.describe 'bin/brain-games', type: :aruba do
   it 'works' do
     expect(last_command_started).to be_stopped
     expect(last_command_started).to be_successfully_executed
-    expect(last_command_started).to have_output(/Welcome to the Brain Games!/)
-    expect(last_command_started).to have_output(/May I have your name?/)
-    expect(last_command_started).to have_output(/Hello, Tirion/)
+    expect(last_command_started).to have_output /Welcome to the Brain Games!/
+    expect(last_command_started).to have_output /May I have your name?/
+    expect(last_command_started).to have_output /Hello, Tirion/
   end
 end
